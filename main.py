18239a21db7871eb0f501f37e49907e7bac99ea9
nolan freeman abbott <nolan@freemanabbott.com>
2023-12-30 02:14:11 -0600

    clean up main file to be used for general testing

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-from Student import Student
 
 def main():
-    courses = {
-        ('Calculus II', 'MATH-2414', 'C', 4, 'Summer 2023', 'Austin Community College'),
-        ('American Sign Language', 'SGNL-1401', 'B', 4, 'Summer 2023', 'Austin Community College'),
-        ('American Sign Language', 'SGNL-1401', 'W', 4, 'Summer 2023', 'Austin Community College'),
-    }
-
-    s = Student(courses)
-
-    print(s.gpa)
-    print(s.completion_pct)
+    pass
 
 if __name__ == '__main__':
     main()
